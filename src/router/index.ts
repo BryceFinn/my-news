5e7466e823f36094aaa3a89518eceb3b23c456c1
bf <baifan.github@outlook.com>
2024-05-16 23:51:27 +0800

    feat(router)：添加editor的各种路由

--- src/router/index.ts
+++ src/router/index.ts
@@ -61,18 +61,118 @@ export const constantRoutes: RouteRecordRaw[] = [
       }
     ]
   },
+  // {
+  //   path: '/unocss',
+  //   component: Layouts,
+  //   redirect: '/unocss/index',
+  //   children: [
+  //     {
+  //       path: 'index',
+  //       component: () => import('@/views/unocss/index.vue'),
+  //       name: 'UnoCSS',
+  //       meta: {
+  //         title: 'UnoCSS',
+  //         svgIcon: 'unocss'
+  //       }
+  //     }
+  //   ]
+  // },
+  // {
+  //   path: '/table',
+  //   component: Layouts,
+  //   redirect: '/table/element-plus',
+  //   name: 'Table',
+  //   meta: {
+  //     title: '表格',
+  //     elIcon: 'Grid'
+  //   },
+  //   children: [
+  //     {
+  //       path: 'element-plus',
+  //       component: () => import('@/views/table/element-plus/index.vue'),
+  //       name: 'ElementPlus',
+  //       meta: {
+  //         title: 'Element Plus',
+  //         keepAlive: true
+  //       }
+  //     },
+  //     {
+  //       path: 'vxe-table',
+  //       component: () => import('@/views/table/vxe-table/index.vue'),
+  //       name: 'VxeTable',
+  //       meta: {
+  //         title: 'Vxe Table',
+  //         keepAlive: true
+  //       }
+  //     }
+  //   ]
+  // },
   {
-    path: '/unocss',
-    component: Layouts,
-    redirect: '/unocss/index',
+    path: '/newspaper',
+    redirect: '/menu/menu1',
+    name: 'NewsPaper',
+    meta: {
+      title: '报纸',
+      svgIcon: 'newspaper'
+    },
     children: [
       {
-        path: 'index',
-        component: () => import('@/views/unocss/index.vue'),
-        name: 'UnoCSS',
+        path: 'wsj',
+        component: () => import('@/views/newspaper/wsj/index.vue'),
+        name: 'WSJ',
         meta: {
-          title: 'UnoCSS',
-          svgIcon: 'unocss'
+          title: '华尔街日报'
+        }
+      },
+      {
+        path: 'nyt',
+        component: () => import('@/views/newspaper/nyt/index.vue'),
+        name: 'NYT',
+        meta: {
+          title: '纽约日报'
+        }
+      },
+      {
+        path: 'ft',
+        component: () => import('@/views/newspaper/ft/index.vue'),
+        name: 'FT',
+        meta: {
+          title: '金融时报'
+        }
+      }
+    ]
+  },
+  {
+    path: '/magazine',
+    redirect: '/menu/menu1',
+    name: 'Magazine',
+    meta: {
+      title: '杂志',
+      svgIcon: 'magazine'
+    },
+    children: [
+      {
+        path: 'te',
+        component: () => import('@/views/magazine/te/index.vue'),
+        name: 'TE',
+        meta: {
+          title: '经济学人'
+        }
+      },
+      {
+        path: 'barron',
+        component: () => import('@/views/magazine/barron/index.vue'),
+        name: 'Barron',
+        meta: {
+          title: '巴伦周刊'
+        }
+      },
+      {
+        path: 'bloomberg',
+        component: () => import('@/views/magazine/bloomberg/index.vue'),
+        name: 'Bloomberg',
+        meta: {
+          title: '彭博商业周刊'
         }
       }
     ]
@@ -80,174 +180,144 @@ export const constantRoutes: RouteRecordRaw[] = [
   {
     path: '/link',
     meta: {
-      title: '外链',
+      title: '联系我',
       svgIcon: 'link'
     },
     children: [
       {
-        path: 'https://juejin.cn/post/7089377403717287972',
+        path: 'https://www.baidu.com',
         component: () => {},
         name: 'Link1',
         meta: {
-          title: '中文文档'
+          title: '微信付款码'
         }
       },
       {
-        path: 'https://juejin.cn/column/7207659644487139387',
+        path: 'https://www.baidu.com',
         component: () => {},
         name: 'Link2',
         meta: {
-          title: '新手教程'
-        }
-      }
-    ]
-  },
-  {
-    path: '/table',
-    component: Layouts,
-    redirect: '/table/element-plus',
-    name: 'Table',
-    meta: {
-      title: '表格',
-      elIcon: 'Grid'
-    },
-    children: [
-      {
-        path: 'element-plus',
-        component: () => import('@/views/table/element-plus/index.vue'),
-        name: 'ElementPlus',
-        meta: {
-          title: 'Element Plus',
-          keepAlive: true
-        }
-      },
-      {
-        path: 'vxe-table',
-        component: () => import('@/views/table/vxe-table/index.vue'),
-        name: 'VxeTable',
-        meta: {
-          title: 'Vxe Table',
-          keepAlive: true
-        }
-      }
-    ]
-  },
-  {
-    path: '/menu',
-    component: Layouts,
-    redirect: '/menu/menu1',
-    name: 'Menu',
-    meta: {
-      title: '多级路由',
-      svgIcon: 'menu'
-    },
-    children: [
-      {
-        path: 'menu1',
-        component: () => import('@/views/menu/menu1/index.vue'),
-        redirect: '/menu/menu1/menu1-1',
-        name: 'Menu1',
-        meta: {
-          title: 'menu1'
-        },
-        children: [
-          {
-            path: 'menu1-1',
-            component: () => import('@/views/menu/menu1/menu1-1/index.vue'),
-            name: 'Menu1-1',
-            meta: {
-              title: 'menu1-1',
-              keepAlive: true
-            }
-          },
-          {
-            path: 'menu1-2',
-            component: () => import('@/views/menu/menu1/menu1-2/index.vue'),
-            redirect: '/menu/menu1/menu1-2/menu1-2-1',
-            name: 'Menu1-2',
-            meta: {
-              title: 'menu1-2'
-            },
-            children: [
-              {
-                path: 'menu1-2-1',
-                component: () => import('@/views/menu/menu1/menu1-2/menu1-2-1/index.vue'),
-                name: 'Menu1-2-1',
-                meta: {
-                  title: 'menu1-2-1',
-                  keepAlive: true
-                }
-              },
-              {
-                path: 'menu1-2-2',
-                component: () => import('@/views/menu/menu1/menu1-2/menu1-2-2/index.vue'),
-                name: 'Menu1-2-2',
-                meta: {
-                  title: 'menu1-2-2',
-                  keepAlive: true
-                }
-              }
-            ]
-          },
-          {
-            path: 'menu1-3',
-            component: () => import('@/views/menu/menu1/menu1-3/index.vue'),
-            name: 'Menu1-3',
-            meta: {
-              title: 'menu1-3',
-              keepAlive: true
-            }
-          }
-        ]
-      },
-      {
-        path: 'menu2',
-        component: () => import('@/views/menu/menu2/index.vue'),
-        name: 'Menu2',
-        meta: {
-          title: 'menu2',
-          keepAlive: true
-        }
-      }
-    ]
-  },
-  {
-    path: '/hook-demo',
-    component: Layouts,
-    redirect: '/hook-demo/use-fetch-select',
-    name: 'HookDemo',
-    meta: {
-      title: 'Hook',
-      elIcon: 'Menu',
-      alwaysShow: true
-    },
-    children: [
-      {
-        path: 'use-fetch-select',
-        component: () => import('@/views/hook-demo/use-fetch-select.vue'),
-        name: 'UseFetchSelect',
-        meta: {
-          title: 'useFetchSelect'
-        }
-      },
-      {
-        path: 'use-fullscreen-loading',
-        component: () => import('@/views/hook-demo/use-fullscreen-loading.vue'),
-        name: 'UseFullscreenLoading',
-        meta: {
-          title: 'useFullscreenLoading'
-        }
-      },
-      {
-        path: 'use-watermark',
-        component: () => import('@/views/hook-demo/use-watermark.vue'),
-        name: 'UseWatermark',
-        meta: {
-          title: 'useWatermark'
+          title: '支付宝口令红包'
         }
       }
     ]
   }
+  // {
+  //   path: '/menu',
+  //   component: Layouts,
+  //   redirect: '/menu/menu1',
+  //   name: 'Menu',
+  //   meta: {
+  //     title: '多级路由',
+  //     svgIcon: 'menu'
+  //   },
+  //   children: [
+  //     {
+  //       path: 'menu1',
+  //       component: () => import('@/views/menu/menu1/index.vue'),
+  //       redirect: '/menu/menu1/menu1-1',
+  //       name: 'Menu1',
+  //       meta: {
+  //         title: 'menu1'
+  //       },
+  //       children: [
+  //         {
+  //           path: 'menu1-1',
+  //           component: () => import('@/views/menu/menu1/menu1-1/index.vue'),
+  //           name: 'Menu1-1',
+  //           meta: {
+  //             title: 'menu1-1',
+  //             keepAlive: true
+  //           }
+  //         },
+  //         {
+  //           path: 'menu1-2',
+  //           component: () => import('@/views/menu/menu1/menu1-2/index.vue'),
+  //           redirect: '/menu/menu1/menu1-2/menu1-2-1',
+  //           name: 'Menu1-2',
+  //           meta: {
+  //             title: 'menu1-2'
+  //           },
+  //           children: [
+  //             {
+  //               path: 'menu1-2-1',
+  //               component: () => import('@/views/menu/menu1/menu1-2/menu1-2-1/index.vue'),
+  //               name: 'Menu1-2-1',
+  //               meta: {
+  //                 title: 'menu1-2-1',
+  //                 keepAlive: true
+  //               }
+  //             },
+  //             {
+  //               path: 'menu1-2-2',
+  //               component: () => import('@/views/menu/menu1/menu1-2/menu1-2-2/index.vue'),
+  //               name: 'Menu1-2-2',
+  //               meta: {
+  //                 title: 'menu1-2-2',
+  //                 keepAlive: true
+  //               }
+  //             }
+  //           ]
+  //         },
+  //         {
+  //           path: 'menu1-3',
+  //           component: () => import('@/views/menu/menu1/menu1-3/index.vue'),
+  //           name: 'Menu1-3',
+  //           meta: {
+  //             title: 'menu1-3',
+  //             keepAlive: true
+  //           }
+  //         }
+  //       ]
+  //     },
+  //     {
+  //       path: 'menu2',
+  //       component: () => import('@/views/menu/menu2/index.vue'),
+  //       name: 'Menu2',
+  //       meta: {
+  //         title: 'menu2',
+  //         keepAlive: true
+  //       }
+  //     }
+  //   ]
+  // },
+  // {
+  //   path: '/hook-demo',
+  //   component: Layouts,
+  //   redirect: '/hook-demo/use-fetch-select',
+  //   name: 'HookDemo',
+  //   meta: {
+  //     title: 'Hook',
+  //     elIcon: 'Menu',
+  //     alwaysShow: true
+  //   },
+  //   children: [
+  //     {
+  //       path: 'use-fetch-select',
+  //       component: () => import('@/views/hook-demo/use-fetch-select.vue'),
+  //       name: 'UseFetchSelect',
+  //       meta: {
+  //         title: 'useFetchSelect'
+  //       }
+  //     },
+  //     {
+  //       path: 'use-fullscreen-loading',
+  //       component: () => import('@/views/hook-demo/use-fullscreen-loading.vue'),
+  //       name: 'UseFullscreenLoading',
+  //       meta: {
+  //         title: 'useFullscreenLoading'
+  //       }
+  //     },
+  //     {
+  //       path: 'use-watermark',
+  //       component: () => import('@/views/hook-demo/use-watermark.vue'),
+  //       name: 'UseWatermark',
+  //       meta: {
+  //         title: 'useWatermark'
+  //       }
+  //     }
+  //   ]
+  // }
 ]
 
 /**
