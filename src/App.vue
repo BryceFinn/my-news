5ac88f4819d6835d107b3bae0193a38a1c51d7d6
fan.bai <fan.bai@woqutech.com>
2024-05-16 17:52:00 +0800

    fix: 登录页面修复与优化
    - 修复验证码图片加载失败时的错误图标显示
    - 优化验证码刷新逻辑，确保每次点击生成新验证码
    - 修复表单验证，增强`code`字段的验证规则
    - 确保输入框焦点切换无误，防止键盘导航异常
    - 调整登录按钮的禁用状态，仅在输入有效时允许提交
    - 优化布局以适应不同屏幕尺寸的响应式设计
    - 解决可能影响用户体验的样式和交互问题

--- src/App.vue
+++ src/App.vue
@@ -9,13 +9,15 @@ const { initTheme } = useTheme()
 /** 初始化主题 */
 initTheme()
 
+const descMsg: string =
+  '<p>睁眼看世界读书会是一个致力于推广阅读和文化交流的平台，通过组织各类读书活动，鼓励人们开阔视野，增长知识，促进思想交流和文化传承。</p>'
+
 /** 作者小心思 */
 ElNotification({
-  title: 'Hello, World!',
+  title: '睁眼看世界读书会',
   type: 'success',
   dangerouslyUseHTMLString: true,
-  message:
-    "<a style='color: teal' target='_blank' href='https://github.com/un-pany/v3-admin-vite'>睁眼看世界读书会，睁眼看世界读书会是一个致力于推广阅读和文化交流的平台，通过组织各类读书活动，鼓励人们开阔视野，增长知识，促进思想交流和文化传承。</a>",
+  message: descMsg,
   duration: 0,
   position: 'bottom-right'
 })
