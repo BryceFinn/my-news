325ae222951162ab87fd6f36d947244c80125a92
fan.bai <fan.bai@woqutech.com>
2024-05-29 14:48:56 +0800

    🐛 (service.ts): fix response handling logic by adding missing import for axios and improving switch case handling for different response codes   📝 (service.ts): add console.log for response and apiData to aid in debugging and understanding the flow of data in the application   ✨ (service.ts): refactor switch case to handle more specific response codes and provide better error messages for unhandled cases

--- src/utils/service.ts
+++ src/utils/service.ts
@@ -1,5 +1,5 @@
-import axios, { type AxiosInstance, type AxiosRequestConfig } from 'axios'
 import { useUserStoreHook } from '@/store/modules/user'
+import axios, { type AxiosInstance, type AxiosRequestConfig } from 'axios'
 import { ElMessage } from 'element-plus'
 import { get, merge } from 'lodash-es'
 import { getToken } from './cache/cookies'
@@ -23,6 +23,7 @@ function createService() {
   // 响应拦截（可根据具体业务作出相应的调整）
   service.interceptors.response.use(
     (response) => {
+      console.log(response)
       // apiData 是 api 返回的数据
       const apiData = response.data
       // 二进制数据则直接返回
@@ -35,17 +36,22 @@ function createService() {
         ElMessage.error('非本系统的接口')
         return Promise.reject(new Error('非本系统的接口'))
       }
-      switch (code) {
-        case 200:
-          // 本系统采用 code === 0 来表示没有业务错误
-          return apiData
-        case 401:
-          // Token 过期时
-          return logout()
-        default:
-          // 不是正确的 code
-          ElMessage.error(apiData.message || 'Error')
-          return Promise.reject(new Error('Error'))
+      if (code === 0) {
+        console.log(apiData)
+        // 本系统采用 code === 0 来表示没有业务错误
+        return apiData
+      } else if (code >= 999) {
+        // 不是正确的 code
+        ElMessage.error(apiData.message || 'Error')
+        return Promise.reject(new Error('Error'))
+      } else if (code < 0) {
+        // Token 过期时
+        ElMessage.error(apiData.message || 'Error')
+        return logout()
+      } else {
+        // 处理其他情况，或者抛出一个错误
+        ElMessage.error('Unhandled code: ' + code)
+        return Promise.reject(new Error('Unhandled code'))
       }
     },
     (error) => {
